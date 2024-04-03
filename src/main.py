82e5dd515e7181b32e6667173a6130c5a2cc4daf
Javen Zamojcin <javen.william@gmail.com>
2024-04-03 16:10:37 -0400

    impl kfolds cross validation trainer

--- src/main.py
+++ src/main.py
@@ -10,7 +10,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from trainer_neat import TrainerNEAT
 from trainer_torch import TrainerTorch
-from torch_geometric.data import Data
+from trainer_geotorch import TrainerGTorch
+from torch_geometric.data import Data, Dataset
 from torch_geometric.utils import from_scipy_sparse_matrix, mask_to_index, unbatch_edge_index, to_edge_index
 from reporter import ResultsReporter
 from torch_geometric.datasets import Planetoid
@@ -20,12 +21,10 @@ from ydata_synthetic.synthesizers import ModelParameters, TrainParameters
 from torch_geometric.transforms import RandomLinkSplit, RandomNodeSplit
 
 
-def train_torch(labels, features, adj, adj_csr, idx_train, idx_test, cora_data):
+def train_torch():
     """
     Preprocess data.
     """
-    # x = features
-
     # dimensionality reduction
     # scaler = StandardScaler()
     # x = scaler.fit_transform(x)
@@ -36,18 +35,19 @@ def train_torch(labels, features, adj, adj_csr, idx_train, idx_test, cora_data):
     # print(x.shape)
 
     # graph aggregated spacial filter
-    x = []
-    for i in range(len(features)):
-        neigh_feats = features[adj_csr[i].astype(bool)]
-        agg_feats = features[i] + neigh_feats.mean(axis=0)  # weighed-mean aggregation
-        # agg_feats = features[i] + neigh_feats.sum(axis=0)  # weighed-sum aggregation
-        x.append(agg_feats)
-    x = np.array(x)
-    scaler = StandardScaler()
-    x = scaler.fit_transform(x)
+    # x = []
+    # for i in range(len(features)):
+    #     neigh_feats = features[adj_csr[i].astype(bool)]
+    #     agg_feats = features[i] + neigh_feats.mean(axis=0)  # weighed-mean aggregation
+    #     # agg_feats = features[i] + neigh_feats.sum(axis=0)  # weighed-sum aggregation
+    #     x.append(agg_feats)
+    # x = np.array(x)
+    # scaler = StandardScaler()
+    # x = scaler.fit_transform(x)
 
     # format data
-    x = torch.from_numpy(x).type(torch.float)
+    x = torch.from_numpy(features).type(torch.float)
+    # x = torch.from_numpy(x).type(torch.float)
     y = torch.zeros(x.shape[0]).type(torch.long)
     y[idx_train] = torch.from_numpy(labels).type(torch.long)
     edges = from_scipy_sparse_matrix(adj)
@@ -55,59 +55,58 @@ def train_torch(labels, features, adj, adj_csr, idx_train, idx_test, cora_data):
     """
     Construct Torch data object(s).
     """
-    train_data = Data(
-        x=x[idx_train],
+    data = Data(
+        x=x,
         y=y,
         edge_index=edges[0],
-        train_mask=torch.zeros(x.shape[0]).type(torch.bool),
-        val_mask=torch.zeros(x.shape[0]).type(torch.bool),
-        test_mask=torch.zeros(x.shape[0]).type(torch.bool),
+        idx_train=idx_train,
+        idx_test=idx_test,
     )
+
     # create data splits
-    transform = RandomNodeSplit(num_test=50, num_val=50)
-    t = transform(train_data)
-    train_data.x = x
-    train_data.train_mask[idx_train] = t.train_mask
-    train_data.val_mask[idx_train] = t.val_mask
-    train_data.test_mask[idx_train] = t.test_mask
+    # transform = RandomNodeSplit(num_test=50, num_val=50)
+    # t = transform(train_data)
+    # train_data.x = x
+    # train_data.train_mask[idx_train] = t.train_mask
+    # train_data.val_mask[idx_train] = t.val_mask
+    # train_data.test_mask[idx_train] = t.test_mask
 
     """
     Synthesize additional training data.
     """
-    # df = pd.DataFrame(data.x.numpy())
+    # d = {'0': train_data.x[idx_train][:, 0].numpy(), 'class': train_data.y[idx_train].numpy()}
+    # df = pd.DataFrame(d)
     # print(df)
     # synth = RegularSynthesizer(modelname='fast')
     # synth.fit(data=df)
     # synth_data = synth.sample(1000)
     # print(synth_data)
+    # exit(1)
 
     """
     Run Trainer.
     """
-    run_id = 48
-    trainer = TrainerTorch(run_id=run_id,)
-    y_pred = trainer.run(
-        train_data=train_data,
+    run_id = 50
+    trainer = TrainerTorch(
+        run_id=run_id,
         lr=0.001,
         weight_decay=5e-4,
-        n_hidden=64,
-        n_epochs=5000,
+        n_epochs=50,
         lr_decay=0.80,
         lr_patience=50,
         epoch_patience=100,
     )
+    y_pred = trainer.run(data=data)
+
     # sort predictions by correct index & save results
     y_pred = y_pred[idx_test]
-    trainer.logger.info(
-        f"val_mask: {train_data.val_mask.sum().item()}, train_mask: {train_data.train_mask.sum().item()}, test_mask: {train_data.test_mask.sum().item()}"
-    )
     trainer.logger.info(f"{x.numpy().shape}")
     print(f"y_pred[:10] = {y_pred[:10].tolist()}")
     print(f"y_real[:10] = [1, 2, 2, 1, 1, 2, 3, 1, 1, 1]")
     np.savetxt(f'logs/submission_{run_id}.txt', y_pred, fmt='%d')
 
 
-def train_neat(labels, features, adj, adj_csr, idx_train, idx_test, cora_data):
+def train_neat():
     """
     Preprocess data.
     """
@@ -151,21 +150,21 @@ if __name__ == "__main__":
     """
     Load data.
     """
-    _labels = np.load('./data_2024/labels.npy')  # (496,) [0-6] training labels
-    _features = np.load('./data_2024/features.npy')  # (2480, 1390) [0-1] node features
+    labels = np.load('./data_2024/labels.npy')  # (496,) [0-6] training labels
+    features = np.load('./data_2024/features.npy')  # (2480, 1390) [0-1] node features
 
-    _adj = sp.load_npz('./data_2024/adj.npz')
-    _adj_csr = np.load('./data_2024/adj.npz')  # (2480, 2480) CSR matrix
-    _adj_csr = csr_matrix((_adj_csr['data'], _adj_csr['indices'], _adj_csr['indptr']), shape=_adj_csr['shape']).toarray()
+    adj = sp.load_npz('./data_2024/adj.npz')
+    adj_csr = np.load('./data_2024/adj.npz')  # (2480, 2480) CSR matrix
+    adj_csr = csr_matrix((adj_csr['data'], adj_csr['indices'], adj_csr['indptr']), shape=adj_csr['shape']).toarray()
 
-    splits = json.load(open('./data_2024/splits.json'))  # ['idx_train', 'idx_test']
-    _idx_train = splits['idx_train']  # (496,)
-    _idx_test = splits['idx_test']  # (1984,)
+    _splits = json.load(open('./data_2024/splits.json'))  # ['idx_train', 'idx_test']
+    idx_train = _splits['idx_train']  # (496,)
+    idx_test = _splits['idx_test']  # (1984,)
 
-    _cora = Planetoid(root='C:\Tmp\Cora', name='Cora')[0]
+    cora_dataset = Planetoid(root='C:\Tmp\Cora', name='Cora')
 
     """
     Run trainer.
     """
-    train_torch(_labels, _features, _adj, _adj_csr, _idx_train, _idx_test, _cora)
+    train_torch()
     # train_neat()
--- src/trainer_torch.py
+++ src/trainer_torch.py
@@ -1,37 +1,33 @@
-import json
-import os
 import time
 import torch
 import torch.nn.functional as F
 from sklearn.model_selection import StratifiedKFold
-from torch_geometric.nn import GCNConv
-from torch_geometric.utils import from_scipy_sparse_matrix
-from torch_geometric.data import Data
 import numpy as np
 import logging
-
-
-class GCN(torch.nn.Module):
-    def __init__(self, num_node_features, num_hidden, num_classes):
-        super().__init__()
-        self.conv1 = GCNConv(num_node_features, num_hidden)
-        self.conv2 = GCNConv(num_hidden, num_classes)
-
-    def forward(self, data):
-        x, edge_index = data.x, data.edge_index
-
-        x = self.conv1(x, edge_index)
-        x = F.relu(x)
-        x = F.dropout(x, training=self.training)
-        x = self.conv2(x, edge_index)
-
-        return F.log_softmax(x, dim=1)
+from torch import tensor
+from torch_geometric.utils import index_to_mask
+from models import GCN, GCN_2L
+from itertools import product
 
 
 class TrainerTorch:
-    def __init__(self, run_id):
+    def __init__(self, run_id, lr=0.01, weight_decay=5e-4, n_epochs=500, lr_decay=0.8, lr_patience=50,
+                 epoch_patience=500):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        # run parameters
         self.run_id = run_id
+        self.lr = lr
+        self.lr_decay = lr_decay
+        self.lr_patience = lr_patience
+        self.weight_decay = weight_decay
+        self.n_epochs = n_epochs
+        self.epoch_patience = epoch_patience
+
+        # cross-validation parameters
+        self.layers = [2, 3, 4, 5]
+        self.hiddens = [16, 32, 64, 128]
+        self.net = GCN
 
         # logging
         self.logger = logging.getLogger()
@@ -39,106 +35,204 @@ class TrainerTorch:
         self.logger.addHandler(logging.FileHandler(f"logs/{run_id}.log"))
         self.logger.addHandler(logging.StreamHandler())
 
-    def run(self, train_data, lr=0.01, weight_decay=5e-4, n_hidden=16, n_epochs=5000, lr_decay=0.8, lr_patience=50, epoch_patience=500):
-        # graph convolutional network model
-        model = GCN(
-            num_node_features=train_data.x.shape[1],
-            num_hidden=n_hidden,
-            num_classes=int((train_data.y.max() + 1).item())
-        ).to(self.device)
+    def run(self, data):
+        # model performance results
+        results = []
+        best_result = (float('inf'), 0, 0)  # (loss, acc, std)
+        best_desc = ""
+        best_pred = None
 
-        # optimizer & scheduler
-        optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
-        lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-            optimizer=optimizer,
-            factor=lr_decay,
-            patience=lr_patience
-        )
+        # log run parameters
+        self.logger.info(f"Run({self.run_id}) parameters:\n\tn_epochs={self.n_epochs},"
+                         f"\n\tlr={self.lr},"
+                         f"\n\tlr_decay={self.lr_decay},"
+                         f"\n\tweight_decay={self.weight_decay},"
+                         f"\n\tcv_layers={self.layers},"
+                         f"\n\tcv_hiddens={self.hiddens},"
+                         f"\n\tcv_models={self.net}\n"
+                         )
 
+        # run & evaluate each model configuration
+        for model_idx, (n_layers, n_hidden) in enumerate(product(self.layers, self.hiddens)):
+            # graph convolutional network model
+            model = self.net(
+                data=data,
+                n_hidden=n_hidden,
+                n_layers=n_layers,
+            )
+            self.logger.info(f"# Evaluating model({model_idx}): n_layers={n_layers}, n_hidden={n_hidden}")
+
+            # cross-validate & train model
+            loss, acc, std = self.cross_validation(
+                data,
+                model,
+                n_folds=10,
+                n_epochs=self.n_epochs,
+                lr=self.lr,
+                lr_decay=self.lr_decay,
+                lr_step_size=50,
+                lr_patience=self.lr_patience,
+                weight_decay=self.weight_decay,
+            )
+
+            # save best model results & predictions
+            if loss < best_result[0]:
+                best_result = (loss, acc, std)
+                best_pred = self.predict(model, data)
+                best_desc = f"Best Model({model_idx}) results: accuracy={best_result[1]:.3f} +/- {best_result[2]:.3f}, n_layers={n_layers}, n_hidden={n_hidden}"
+
+            desc = f"accuracy={acc:.3f} +/- {std:.3f}, n_layers={n_layers}, n_hidden={n_hidden}"
+            self.logger.info(f"Model({model_idx}) results: {desc}")
+            self.logger.info(f'{best_desc}\n')
+            results += [f'{model}({model_idx}): {desc}']
+
+        # results from all models
+        results = '\n'.join(results)
+        self.logger.info(f'--\n{results}')
+        return best_pred
+
+    def cross_validation(self, data, model, n_epochs, n_folds, lr, weight_decay, lr_decay, lr_patience, lr_step_size):
+        val_losses, accs, durations = [], [], []
+        x_len = len(data.idx_train)
+
+        # train & evaluate using k-folds
+        for fold, (train_idx, test_idx, val_idx) in enumerate(zip(*self.k_fold(data, n_folds))):
+
+            # prepare fold data masks
+            data.train_mask = torch.zeros(data.x.shape[0], dtype=torch.bool)
+            data.train_mask[data.idx_train] = index_to_mask(train_idx, size=x_len)
+
+            data.val_mask = torch.zeros(data.x.shape[0], dtype=torch.bool)
+            data.val_mask[data.idx_train] = index_to_mask(val_idx, size=x_len)
+
+            data.test_mask = torch.zeros(data.x.shape[0], dtype=torch.bool)
+            data.test_mask[data.idx_train] = index_to_mask(test_idx, size=x_len)
+
+            model.to(self.device).reset_parameters()
+
+            # optimizer & scheduler
+            optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
+            lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+                optimizer=optimizer,
+                factor=lr_decay,
+                patience=lr_patience
+            )
+
+            # train & evaluate model for this fold
+            t_start = time.perf_counter()
+            for epoch in range(1, n_epochs + 1):
+                train_loss = self.train(model, optimizer, data)
+                val_losses.append(self.eval_loss(model, data))
+                accs.append(self.eval_accuracy(model, data))
+
+                # log epoch results
+                if epoch % 10 == 0:
+                    eval_info = {
+                        'fold': fold,
+                        'epoch': epoch,
+                        'train_loss': train_loss,
+                        'val_loss': val_losses[-1],
+                        'test_acc': accs[-1],
+                    }
+                    self.logger.info(eval_info)
+
+                if epoch % lr_step_size == 0:
+                    for param_group in optimizer.param_groups:
+                        param_group['lr'] = lr_decay * param_group['lr']
+
+            # cuda optimization
+            if torch.cuda.is_available():
+                torch.cuda.synchronize()
+            elif hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
+                torch.mps.synchronize()
+
+            t_end = time.perf_counter()
+            durations.append(t_end - t_start)
+
+        # final model performance
+        loss, acc, duration = tensor(val_losses), tensor(accs), tensor(durations)
+        loss, acc = loss.view(n_folds, n_epochs), acc.view(n_folds, n_epochs)
+        loss, argmin = loss.min(dim=1)
+        acc = acc[torch.arange(n_folds, dtype=torch.long), argmin]
+
+        loss_mean = loss.mean().item()
+        acc_mean = acc.mean().item()
+        acc_std = acc.std().item()
+        duration_mean = duration.mean().item()
+        logging.info(f'Val Loss: {loss_mean:.4f}, Test Accuracy: {acc_mean:.3f} '
+                     f'+/- {acc_std:.3f}, Duration: {duration_mean:.3f}')
+
+        return loss_mean, acc_mean, acc_std
+
+    def train(self, model, optimizer, data):
         """
-        Train Model.
+        Train model.
         """
-        vlss_mn = np.inf
-        vacc_mx = 0.0
-        state_dict_early_model = None
-        curr_step = 0
+        model.train()
+        optimizer.zero_grad()
+        data = data.to(self.device)
 
-        dur = []
-        for epoch in range(n_epochs):
-            t0 = time.time()
+        out = model(data)
+        out_logp = F.log_softmax(out, 1)
+        loss = F.nll_loss(out_logp[data.train_mask], data.y[data.train_mask])
 
-            # TRAIN
-            model.train()
-            train_logits = model(train_data)
-            train_logp = F.log_softmax(train_logits, 1)
-            train_loss = F.nll_loss(train_logp[train_data.train_mask], train_data.y[train_data.train_mask])
-            train_pred = train_logp.argmax(dim=1)
-            train_acc = torch.eq(train_pred[train_data.train_mask], train_data.y[train_data.train_mask]).float().mean().item()
+        loss.backward()
+        optimizer.step()
+        return loss.item()
 
-            optimizer.zero_grad()
-            train_loss.backward()
-            optimizer.step()
-
-            # VALIDATE
-            model.eval()
-            with torch.no_grad():
-                val_logits = model(train_data)
-                val_logp = F.log_softmax(val_logits, 1)
-                val_loss = F.nll_loss(val_logp[train_data.val_mask], train_data.y[train_data.val_mask]).item()
-                val_pred = val_logp.argmax(dim=1)
-                val_acc = torch.eq(val_pred[train_data.val_mask], train_data.y[train_data.val_mask]).float().mean().item()
-
-            lr_scheduler.step(val_loss)
-            dur.append(time.time() - t0)
-            self.logger.info(
-                "Epoch {:05d} | Train Loss {:.4f} | Train Acc {:.4f} | Val Loss {:.4f} | Val Acc {:.4f} | Time(s) {:.4f}".format(
-                    epoch, train_loss.item(), train_acc, val_loss, val_acc, sum(dur) / len(dur)))
-
-            # Adapted from https://github.com/PetarV-/GAT/blob/master/execute_cora.py
-            if val_acc >= vacc_mx or val_loss <= vlss_mn:
-                if val_acc >= vacc_mx and val_loss <= vlss_mn:
-                    state_dict_early_model = model.state_dict()
-                vacc_mx = np.max((val_acc, vacc_mx))
-                vlss_mn = np.min((val_loss, vlss_mn))
-                curr_step = 0
-            else:
-                curr_step += 1
-                if curr_step >= epoch_patience:
-                    break
-
+    def eval_loss(self, model, data):
         """
-        Evaluate Model.
+        Validate model.
         """
         model.eval()
+        data = data.to(self.device)
         with torch.no_grad():
-            test_logits = model(train_data)
-            test_logp = F.log_softmax(test_logits, 1)
-            test_loss = F.nll_loss(test_logp[train_data.test_mask], train_data.y[train_data.test_mask]).item()
-            test_pred = test_logp.argmax(dim=1)
-            test_acc = torch.eq(test_pred[train_data.test_mask], train_data.y[train_data.test_mask]).float().mean().item()
-            self.logger.info("Test Loss {:.4f} | Test Acc {:.4f}".format(test_loss, test_acc))
+            out = model(data)
+            out_logp = F.log_softmax(out, 1)
+            loss = F.nll_loss(out_logp[data.val_mask], data.y[data.val_mask]).item()
+            return loss
 
-        self.logger.info(
-            f"lr={lr}, weight_decay={weight_decay}, n_hidden={n_hidden}, n_epochs={n_epochs}, lr_decay={lr_decay}, lr_patience={lr_patience}, epoch_patience={epoch_patience}"
-        )
-        model.load_state_dict(state_dict_early_model)
+    def eval_accuracy(self, model, data):
+        """
+        Test model.
+        """
         model.eval()
+        data = data.to(self.device)
         with torch.no_grad():
-            eval_logits = model(train_data)
-            eval_logp = F.log_softmax(eval_logits, 1)
-            eval_pred = eval_logp.argmax(dim=1)
-            return eval_pred
+            out = model(data)
+            out_logp = F.log_softmax(out, 1)
+            pred = out_logp.argmax(dim=1)
+            accuracy = torch.eq(pred[data.test_mask], data.y[data.test_mask]).float().mean().item()
+            return accuracy
 
-    def k_fold(self, dataset, n_folds: int):
+    def k_fold(self, data, n_folds: int):
         skf = StratifiedKFold(n_folds, shuffle=True, random_state=12345)
         test_indices, train_indices = [], []
-        for _, idx in skf.split(torch.zeros(len(dataset)), dataset.data.y):
+        x_len = len(data.idx_train)
+
+        # test index folds
+        for _, idx in skf.split(torch.zeros(x_len), data.y[data.idx_train]):
             test_indices.append(torch.from_numpy(idx).to(torch.long))
+
+        # val index folds
         val_indices = [test_indices[i - 1] for i in range(n_folds)]
 
+        # train index folds
         for i in range(n_folds):
-            train_mask = torch.ones(len(dataset), dtype=torch.bool)
+            train_mask = torch.ones(x_len, dtype=torch.bool)
             train_mask[test_indices[i]] = 0
             train_mask[val_indices[i]] = 0
             train_indices.append(train_mask.nonzero(as_tuple=False).view(-1))
+
         return train_indices, test_indices, val_indices
+
+    def predict(self, model, data):
+        """
+        Final best-model evaluation.
+        """
+        model.eval()
+        with torch.no_grad():
+            out = model(data)
+            out_logp = F.log_softmax(out, 1)
+            pred = out_logp.argmax(dim=1)
+            return pred
